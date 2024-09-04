d78c01721e205a55765907ddc71dd05703fd15cd
Yhonier182 <jhonier18249@gmail.com>
2024-09-03 19:01:47 -0500

    uso de inputs para mostrar lista productos

--- src/app/components/card-product/card-product.component.ts
+++ src/app/components/card-product/card-product.component.ts
@@ -1,44 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-product',
   templateUrl: './card-product.component.html',
   styleUrl: './card-product.component.scss'
 })
-export class CardProductComponent  implements OnInit{
+export class CardProductComponent implements OnInit{
+// creacion del Input que recibe del compoente padre el array de productos
+  @Input() Product = {
+    name: '',
+    price: 0,
+    description: '',
+    inventory: 0,
+    image: ''
+  };
+  
+  cantidad: number = 0;
+  soldOut: boolean = false;
 
-     cantidad: number =0 ;
-     soldOut: boolean = false; //variable creada
 
-  // inicio objecto  de producto
-  Product = {
-    name :'Bike',
-    price :120,
-    inventory :10,
-    image :'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-   
+  constructor() { }
+
+  ngOnInit() {
   }
 
-  
-
-  // fin objeto de producto 
-  //creamos el constructor
-  constructor(){}
-    ngOnInit(){}
-    
-
-    addProduct() {
-      if(this.cantidad < this.Product.inventory){
-        this.soldOut = false;
-        this.cantidad++;
-      } else {
-        this.soldOut = true;
-      }
+  addProduct() {
+    if(this.cantidad < this.Product.inventory){
+      this.soldOut = false;
+      this.cantidad++;
+    } else {
+      this.soldOut = true;
     }
-    removeProduct() {
-      if(this.cantidad > 0){
-        this.soldOut = false;
-        this.cantidad--;
-      }
+  }
+
+  removeProduct() {
+    if(this.cantidad > 0){
+      this.soldOut = false;
+      this.cantidad--;
     }
+  }
+
 }
