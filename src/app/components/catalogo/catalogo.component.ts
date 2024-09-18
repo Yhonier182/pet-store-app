c6f36d1a031b62ceabf3dbe42693a5d0f494d232
Yhonier182 <jhonier18249@gmail.com>
2024-09-17 19:57:28 -0500

    actualizacion 1.0 modificando catalogo component

--- src/app/components/catalogo/catalogo.component.ts
+++ src/app/components/catalogo/catalogo.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import { CartServicesService } from '../../services/cart-services.service';
+
 
 @Component({
   selector: 'app-catalogo',
@@ -9,12 +9,15 @@ import { CartServicesService } from '../../services/cart-services.service';
   styleUrls: ['./catalogo.component.scss']
 })
 export class CatalogoComponent implements OnInit {
+addToCart($event: any) {
+throw new Error('Method not implemented.');
+}
 
   public productos: any[] = [];
 
   constructor(
     private apiService: ApiService,
-    private cartServicesService: CartServicesService
+   
   ) { }
 
   ngOnInit(): void {
@@ -24,112 +27,8 @@ export class CatalogoComponent implements OnInit {
         });
   }
 
-  
-  
 
 
-  Products = [
-    {
-    id:1,
-    name: 'Bike_1',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  },
-  {
-    id:2,
-    name: 'Bike_2',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  },
-  {
-    id:3,
-    name: 'Bike_3',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  }, 
-  {
-    id:4,
-    name: 'Bike_4',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  },
-  {
-    id:5,
-    name: 'Bike_5',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  }, 
-  {
-    id:6,
-    name: 'Bike_6',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  },
-  {
-    id:7,
-    name: 'Bike_7',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  }, 
-  {
-    id:8,
-    name: 'Bike_8',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  },
-  {
-    id:9,
-    name: 'Bike_9',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  }, 
-  {
-    id:10,
-    name: 'Bike_10',
-    price: 120,
-    description: 'Product Description',
-    inventory: 10,
-    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    cantidad: 0
-  },];
-
-
-  
-  // actualizar el método para añadir un producto al carrito mediante el servicio
-  addToCart(product: any) { 
-	  // utilizacion del metodo addTocart del servicio cartServicesService 
-    this.cartServicesService.addToCart(product)
-    // utilizacion del metodo getCart del servicio cartServicesService 
-    //para mostrar en el console.log los producto añadidos
-    console.log( this.cartServicesService.getCart() )
-  }
 }
 
 
